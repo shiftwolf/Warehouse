318ac5b507c5d417ecee362a13da1bfc44b8cdfd
Kueadev <sarahboeckel999@gmail.com>
2022-01-14 11:45:57 +0100

    clean up

--- app/app/src/main/java/com/example/warehouse/z/Order.kt
+++
@@ -1,9 +0,0 @@
-package com.example.warehouse.z
-
-data class OrderPreview(
-    val id: Int,
-    val completed: Int,
-    val customer_id: Int,
-    val employee_id: Int,
-    val created_at: String
-)
